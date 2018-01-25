bf7948292e4e28233d38381504cd461ff7d1cbb9
Z Yin <Dr.Z.Yin@gmail.com>
2018-01-25 14:55:57 -0500

    still figuring out

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
 initializeFirebase();
+var defaultLines = [{
+  lineName: "Southern Dream",
+  destination: "Sydney",
+  maidenVoyage: "1977-01-15-08:00",
+  frequency: "20m"
+}, {
+  lineName: "Furikake",
+  destination: "Tokyo",
+  maidenVoyage: "1989-12-22-15:30",
+  frequency: "2h30m"
+}, {
+  lineName: "Breakaway",
+  destination: "Iceland",
+  maidenVoyage: "2008-07-13-14:00",
+  frequency: "8h"
+}, {
+  lineName: "Lightning Bolt",
+  destination: "Jupiter",
+  maidenVoyage: "2017-08-08-7:07",
+  frequency: "24h"
+}];
+a = "1437";
+// console.log(moment(a, 'm').format("dddd, MMMM Do YYYY, h:mm:ss a"));
+var database = firebase.database().ref('trainLines');
+$('#reset').on('click', resetTimetable);
+database.on('value', redrawTimetable);
 
+function redrawTimetable(snapshotFromFirebase) {
+  for (let i of snapshotFromFirebase.val()) {
+    let newRow = $('<tr>');
+    newRow.append(`<td>${i.lineName}</td>)`);
+    newRow.append(`<td>${i.destination}</td>)`);
+    newRow.append(`<td>${i.frequency}</td>)`);
+    $('#timetable__body').append(newRow);
+  }
+}
+
+function resetTimetable() {
+  database.set(defaultLines);
+}
 
 function initializeFirebase() {
   var apiKey1stHalf = 'AIzaSyCZskGEWNvuDsAq';
@@ -12,6 +51,5 @@ function initializeFirebase() {
     storageBucket: "train-schedule-homework-99e78.appspot.com",
     messagingSenderId: "924815602497"
   };
-  console.log(config);
   firebase.initializeApp(config);
 }
