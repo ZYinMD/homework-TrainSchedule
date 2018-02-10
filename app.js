1708c8952092845820abf4511580637b001009eb
Z Yin <Z.Yin.MD@gmail.com>
2018-02-09 22:52:28 -0500

    basic funtion finished

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
 var localDB;
 initializeFirebase();
-
-
-
-/*
-function writeNewPost(uid, username, title, body) {
-  // A post entry.
-  var postData = {
-    author: username,
-    uid: uid,
-    body: body,
-    title: title,
-    starCount: 0,
-  };
-
-  // Get a key for a new Post.
-  var newPostKey = firebase.database().ref().child('posts').push().key;
-
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  updates['/posts/' + newPostKey] = postData;
-  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-  return firebase.database().ref().update(updates);
-}
-*/
-
-$('#reset').on('click', resetTimetable); //when "reset" button is clicked, reset Firebase to default
-firebase.database().ref('trainLines').on('value', redrawTimetable); //when Firebase detects any changes, update the page
-$('#submit-new-line').on('click', submitNewLine); //when the 'add' button in the modal is clicked, add a new line
-
+$('#reset').on('click', resetFirebase); //when "reset" button is clicked, reset Firebase to default
+$('#submit-new-line').on('click', submitNewLine); //when the 'add' button in the modal is clicked, add a new train line into Firebase
+firebase.database().ref('trainLines').on('value', updateData); //when Firebase detects any changes, update the local database
 function submitNewLine() {
   var newLine = {
     lineName: $('#input-line-name').val(),
@@ -38,60 +11,115 @@ function submitNewLine() {
     maidenVoyageTime: $('#maiden-voyage-time').val(),
     frequency: $('#frequency__hours').val() + 'h' + $('#frequency__minutes').val() + 'm'
   };
-  console.log(newLine);
   firebase.database().ref('trainLines').push(newLine);
 }
 
-function parseLine() {
-  var date = $('#maiden-voyage-date').val(); //get date from input in the format of 'yyyy-mm-dd'
-  date = date.split('-'); //split the date into an array
-  date[1] -= 1; //minus the month by 1, because months count from 0 to 11 in the js date object
-  var time = $('#maiden-voyage-time').val(); //get time from input in the format of 'hh:mm' (24h)
-  time = time.split(':'); //split the time into an array
-  //[...date, ...time],
+function updateData(snapshotFromFirebase) { //this function is triggered on Firebase change. It updates the LocalDB, as well as the display
+  localDB = snapshotFromFirebase.val();
+  updateTimes();
+  setInterval(updateTimes, 1000)
 }
 
-function resetTimetable() { //this function resets Firebase to default
+function updateTimes() { //this function takes data from LocalDB and refresh the timetable on display
+  $('#timetable__body').empty(); //clear current display
+  for (let i in localDB) {
+    let newRow = $('<tr>'); //create a new row in the table, and start appending cells
+    newRow.append(`<td>${localDB[i].lineName}</td>)`);
+    newRow.append(`<td>${localDB[i].destination}</td>)`);
+    newRow.append(`<td>${localDB[i].frequency}</td>)`);
+    //construct a js date object of the start time of the first train
+    let maidenDate = localDB[i].maidenVoyageDate.split('-');
+    maidenDate[1] -= 1; //minus the month by 1, because months count from 0 to 11 in the js date object
+    let maidenTime = localDB[i].maidenVoyageTime.split(':');
+    let maidenVoyage = new Date(...maidenDate, ...maidenTime);
+    //compare the maidenVoyage with now
+    let now = new Date();
+    // the following big chunk of code is to get calculate two key values, nextTrian (in a js date object), and timeAway (in milliseconds)
+    let nextTrain, timeAway, interval, intervalH, intervalM
+    if (now < maidenVoyage) { //if maiden voyage is after now, just calculate how soon
+      timeAway = maidenVoyage - now;
+      nextTrain = maidenVoyage;
+    } else { //if maiden voyage is before now, we're currently in between two trains
+      interval = localDB[i].frequency;
+      if (interval.includes('h')) {
+        intervalH = interval.slice(0, interval.indexOf('h'));
+        intervalM = interval.slice(interval.indexOf('h') + 1, -1);
+        interval = intervalH * 60 * 60 * 1000 + intervalM * 60 * 1000;
+      } else {
+        intervalM = interval.slice(0, -1);
+        interval = intervalM * 60 * 1000;
+      }
+      timeAway = interval - (now - maidenVoyage) % interval;
+      nextTrain = new Date(now - -timeAway); //intention was now + timeAway, but + creates concatenation, so have to use a workaround
+    }
+    timeAway = moment.duration(timeAway);
+    nextTrain = moment(nextTrain);
+    if (timeAway > 60 * 60 * 24 * 1000) { //if timeAway > 1 day
+      timeAway = timeAway.humanize();
+      if (nextTrain.getFullYear() == now.getFullYear()) { //if next train is in this year
+        nextTrain = nextTrain.format('MMM D h:mma');
+      } else { //if next train is not in this year
+        nextTrain = nextTrain.format('MMM D YYYY h:mma');
+      }
+    } else { //if timeAway < 1 day
+      if (timeAway.hours()) { //if hour > 0
+        timeAway = `${timeAway.hours()}:${String(timeAway.minutes()).padStart(2, '0')}:${String(timeAway.seconds()).padStart(2, '0')}` //sometimes minutes and seconds are in single digit when < 10, pad it
+      } else if (timeAway.minutes()) { //if minutes > 0
+        timeAway = `${timeAway.minutes()}:${String(timeAway.seconds()).padStart(2, '0')}`
+      } else {
+        timeAway = String(timeAway.seconds()).padStart(2, '0')
+      }
+      let isTomorrow = nextTrain.date() != now.getDate(); //although timeAway < 1 day, but is it tomorrow?
+      if (nextTrain.minutes()) {
+        nextTrain = nextTrain.format('h:mma'); //show minutes only when it's not 0, e.g. show 5pm instead of 5:00pm
+      } else {
+        nextTrain = nextTrain.format('ha');
+      }
+      if (isTomorrow) {
+        nextTrain = 'tomorrow ' + nextTrain;
+      }
+    }
+    newRow.append(`<td>${nextTrain}</td>)`);
+    newRow.append(`<td>${timeAway}</td>)`);
+    $('#timetable__body').append(newRow); //finally done formatting the row, append it!
+  }
+}
+
+function resetFirebase() { //this function resets Firebase to the initial default value
   var defaultLines = [{
+    lineName: "Class",
+    destination: "1101 K St NW",
+    maidenVoyageDate: "2017-11-07",
+    maidenVoyageTime: "18:30",
+    frequency: "5m"
+  }, {
     lineName: "Southern Dream",
     destination: "Sydney",
     maidenVoyageDate: "1977-01-15",
     maidenVoyageTime: "08:00",
-    frequency: "20m"
+    frequency: "15m"
   }, {
     lineName: "Furikake",
     destination: "Tokyo",
     maidenVoyageDate: "1989-12-22",
-    maidenVoyageTime: "15:30",
+    maidenVoyageTime: "15:00",
     frequency: "2h30m"
   }, {
     lineName: "Breakaway",
     destination: "Iceland",
     maidenVoyageDate: "2008-07-13",
     maidenVoyageTime: "14:00",
-    frequency: "8h"
+    frequency: "4h"
   }, {
     lineName: "Lightning Bolt",
     destination: "Jupiter",
     maidenVoyageDate: "2017-08-08",
-    maidenVoyageTime: "07:07",
+    maidenVoyageTime: "11:00",
     frequency: "24h"
   }];
-
   firebase.database().ref('trainLines').set({}); //empty everything in firebase
   for (let i of defaultLines) {
     firebase.database().ref('trainLines').push(i);
-  }
-}
-
-function redrawTimetable(snapshotFromFirebase) { //this function updates the timetable on the page using Firebase data, also updates the LocalDB
-  localDB = snapshotFromFirebase.val();
-  for (let i in localDB) {
-    let newRow = $('<tr>');
-    newRow.append(`<td>${localDB[i].lineName}</td>)`);
-    newRow.append(`<td>${localDB[i].destination}</td>)`);
-    newRow.append(`<td>${localDB[i].frequency}</td>)`);
-    $('#timetable__body').append(newRow);
   }
 }
 
@@ -108,5 +136,3 @@ function initializeFirebase() {
   };
   firebase.initializeApp(config);
 }
-
-
